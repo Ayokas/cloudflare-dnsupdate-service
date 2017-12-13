83e6d4877d8d123679e614917b61053334276105
Darth.Ayokas <simon.ameling@yahoo.de>
2017-12-13 17:33:15 +0100

    Spring Boot Starter project

---
+++ src/main/java/org/ayokas/cloudflarednsupdateservice/CloudflareDnsUpdateServiceApplication.java
@@ -0,0 +1,12 @@
+package org.ayokas.cloudflarednsupdateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudflareDnsUpdateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudflareDnsUpdateServiceApplication.class, args);
+	}
+}
